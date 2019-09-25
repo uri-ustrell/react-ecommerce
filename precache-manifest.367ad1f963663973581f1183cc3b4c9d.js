bd9fd1dff66e4ca87dc0c77d7b4a058ced5aa6e2
root <root@DESKTOP-QRLQ7Q8.localdomain>
2019-09-25 23:59:05 +0200

    Updates

---
+++ precache-manifest.367ad1f963663973581f1183cc3b4c9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac944f87f8d30125676b5b15ca280358",
+    "url": "/react-ecommerce/index.html"
+  },
+  {
+    "revision": "6199939b24b81ceb2ac1",
+    "url": "/react-ecommerce/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "97c6955537abf3130a74",
+    "url": "/react-ecommerce/static/css/main.7b58bb10.chunk.css"
+  },
+  {
+    "revision": "6199939b24b81ceb2ac1",
+    "url": "/react-ecommerce/static/js/2.76e9b222.chunk.js"
+  },
+  {
+    "revision": "97c6955537abf3130a74",
+    "url": "/react-ecommerce/static/js/main.6e87ce15.chunk.js"
+  },
+  {
+    "revision": "9906ea4f43975063360b",
+    "url": "/react-ecommerce/static/js/runtime-main.d596c8a4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-ecommerce/static/media/logo.25bf045c.svg"
+  }
+]);
